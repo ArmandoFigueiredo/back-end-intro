8cda8a7147abf56d36b2d4712b573c1ae226d583
ArmandoFigueiredo <armandopfigueiredo@gmail.com>
2022-09-19 17:52:10 -0300

    commit

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { express } from "express";
+import express from "express";
 import { foodRouter } from "./routes/food.routes.js"
 
 const app = express();
@@ -6,6 +6,10 @@ const app = express();
 app.use(express.json());
 
 app.use("/food", foodRouter);
+
+app.get("/", (req, res) => {
+    return res.json("Você deu GET em: http://localhost:4000/");
+});
 
 app.listen(4000, () => {
     console.log("rodando ok");
